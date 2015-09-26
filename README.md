d487034c0e241a0d994f9499c81ef1ac44cf46bf
Michael Colynuck <mcolynuck@gmail.com>
2015-09-26 10:14:43 -0700

    Added gitignore, license and readme files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Awfulizer
+This is a utility to awfulize text to passive-aggressively respond to those individuals who always seem to want to use ALL CAPS TO WRITE EMAILS AND TEXT!
+
+The text provided is mangled so that letter case is randomized.  
+
+## Options
+Option swtiches provide the following additional functionality:
+-Randomly convert some upper-case letters to a number that looks similar. (i.e. S to 5)
+-Randomly drop vowels
+
+## Setup
+This is a static website that does not need a server to run and libraries are loaded from CDN sites.
---
+++ awfulize.js
@@ -0,0 +1,144 @@
+(function(awfulize){
+
+	var _inputElm = null,		// Input element to get/set text.
+		_outputElm = null,
+		_options = {
+				numbers: false,		// Defaults
+				vowels: false
+			};
+
+	// Random true/false
+	function randomBoolean() {
+		return (Math.floor((Math.random() * 10) + 1) % 2) == 0 ? true : false;
+	}
+
+
+	// Change case of text randomly.
+	// Maintains Twitter hashtag case.
+	function randomizeCase(text) {
+		var output = "",
+			ignoreWord = false,
+			chr = '';
+
+		for ( var i = 0; i < text.length; i++ ) {
+			chr = text.charAt(i);
+
+			// Ignore words starting with '#' which are Twitter hashtags.
+			if(ignoreWord && chr === ' '){
+				ignoreWord = false;
+			}
+
+			if(!ignoreWord && chr === '#') {
+				ignoreWord = true;
+			}
+
+			if(!ignoreWord) {
+				if(randomBoolean()) {
+					output += chr.toUpperCase();
+				} else {
+					output += chr.toLowerCase();
+				}				
+			} else {
+				output += chr;
+			}
+  		}
+
+  		return output;
+	}
+
+
+
+	// Replaces some captial letters with numbers that look similar.
+	function replaceTextWithNums(text) {
+		return text.replace(/I/g, "1")
+			//.replace(/E/g, "3")
+			.replace(/S/g, "5")
+			.replace(/B/g, "8")
+			//.replace(/P/g, "9")
+			.replace(/O/g, "0");
+	}
+
+
+	// Removes random vowels from text.
+	function dropSomeVowels (text) {
+		var output = "";
+		for ( var i = 0; i < text.length; i++ ) {
+			chr = text.charAt(i);
+			if(chr.match(/[aeiou]/gi) !== null) {		// If vowel?
+				if(randomBoolean()) {					// If true, keep it
+					output += chr;
+				}
+			} else {
+				output += chr;		// constanant
+			}
+		}
+		return output;
+	}
+
+
+
+	// Process text string.
+	function _awfulize(text, options) {
+		var output = "",
+			opts = _options;
+
+		if (options !== undefined && options instanceof Object) {
+			opts = options;
+		}
+
+		// Mix case of text
+		output = randomizeCase(text);
+
+		// Process optional changes
+		if (opts.numbers) {
+			output = replaceTextWithNums(output);
+		}
+
+		if (opts.vowels) {
+			output = dropSomeVowels(output);
+		}
+
+		return output;
+	}
+
+
+
+	// Set options to use
+	awfulize.setOptions = function(configObj) {
+		_options = configObj;
+	}
+
+
+
+	// Define which element to use for input.
+	awfulize.setInput = function(elm) {
+		_inputElm = elm;
+	};
+
+
+	// Define which element to use for output.
+	awfulize.setOutput = function(elm) {
+		_outputElm = elm;
+	}
+
+
+
+	// Start processing the input data
+	awfulize.run = function() {
+		if (!_inputElm) {
+			throw "No input element configured";
+		}
+		if (!_outputElm) {
+			throw "No output element configured";
+		}
+
+		// Get text to process
+		var text = _inputElm[0].value;
+
+		var output = _awfulize(text);
+
+		// Send output to element
+		_outputElm.val(output);
+	};
+
+})(window.awfulize = window.awfulize || {});
